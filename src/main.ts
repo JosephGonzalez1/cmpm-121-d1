277d3b1bb5a536b29f57ff695366a9a71e3158f2
JosephGonzalez1 <152119091+JosephGonzalez1@users.noreply.github.com>
2025-10-10 15:53:03 -0700

    S02 -  Real time Developlment added comments Co-authored-by: wimgonza <wimgonza@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import fireEmoji from "./Fire.jpg";
 import "./style.css";
 
+// Initializes counter
 let counter: number = 0;
 
+// Creation of basic html structure
 document.body.innerHTML = `
   <p>Counter: <span id="counter">0</span></p>
   <button id =  increment> Fire: <img src="${fireEmoji}" class="icon" /></button>
 `;
 
+// Adds click handler
 const button = document.getElementById("increment")!;
 const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
+  // Increments counter by 1
   counter++;
   console.log("Jenalee was here :p"); // Jenalee Nguyen jnguy405@ucsc.edu
+  // Updates counter text
   counterElement.textContent = counter.toString();
 });
